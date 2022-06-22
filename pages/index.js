baa789e25addaf54ade2e81a91b5e151e23bc8ab
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-06-22 18:44:37 +0200

    Adjust head tag

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title></title>
-        <meta name='description' content='' />
+        <title>Job Board</title>
+        <meta name='description' content='Job Board' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <h1>Welcome!</h1>
     </div>
-  )
+  );
 }
